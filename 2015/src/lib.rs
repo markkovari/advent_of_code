e6809cff767047e98c3345699162e0f59a4f14d0
markkovari <kovarimarkofficial@gmail.com>
2023-05-14 10:23:14 +0200

    feat: add 2015 1 first

--- 2015/src/lib.rs
+++ 2015/src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn solve_first(content: &str) -> i32 {
+    let mut result: i32 = 0;
+    for c in content.chars() {
+        if c == '(' {
+            result += 1;
+        } else if c == ')' {
+            result -= 1;
+        }
+    }
+    result
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    const EXAMPLE: &str = include_str!("1_test.txt");
+    const PROD: &str = include_str!("1_prod.txt");
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn it_works_example() {
+        let expected = 3;
+        let result = solve_first(&EXAMPLE);
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn it_works_prod() {
+        let expected = 232;
+        let result = solve_first(&PROD);
+        assert_eq!(expected, result);
     }
 }
