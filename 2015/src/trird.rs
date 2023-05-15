5252ab8304e5b60d13ebe622241b264b146f1253
markkovari <kovarimarkofficial@gmail.com>
2023-05-15 19:28:05 +0200

    feat: 2015 aoc 3rd day first

---
+++ 2015/src/trird.rs
@@ -0,0 +1,114 @@
+use std::collections::HashMap;
+
+use crate::{Excercise, Solvable};
+
+type Position = (i32, i32);
+
+struct Santa {
+    position: Position,
+    visited: HashMap<Position, usize>,
+}
+
+impl Santa {
+    pub fn new() -> Self {
+        let mut visited = HashMap::new();
+        visited.insert((0, 0), 1);
+        Self {
+            position: (0, 0),
+            visited,
+        }
+    }
+    pub fn move_to(&mut self, direction: char) {
+        match direction {
+            '>' => {
+                self.position.0 += 1;
+            }
+            '<' => {
+                self.position.0 -= 1;
+            }
+            '^' => {
+                self.position.1 -= 1;
+            }
+            'v' => {
+                self.position.1 += 1;
+            }
+            _ => {}
+        }
+        let counter = self.visited.entry(self.position).or_insert(0);
+        *counter += 1;
+    }
+
+    pub fn get_visited_multiple(&self) -> usize {
+        let visiteds = self
+            .visited
+            .values()
+            .filter(|&x| *x >= 1)
+            .collect::<Vec<_>>();
+        visiteds.len()
+    }
+}
+
+struct ThirdDay {
+    exercise: Excercise,
+}
+
+impl Solvable for ThirdDay {
+    fn solve_first(&self, is_prod: bool) -> i32 {
+        if is_prod {
+            self.first(self.exercise.content.to_owned())
+        } else {
+            self.first(self.exercise.example.to_owned())
+        }
+    }
+
+    fn solve_second(&self, is_prod: bool) -> i32 {
+        if is_prod {
+            self.second(self.exercise.content.to_owned())
+        } else {
+            self.second(self.exercise.example.to_owned())
+        }
+    }
+    fn first(&self, content: String) -> i32 {
+        let mut santa = Santa::new();
+        for direction in content.chars() {
+            santa.move_to(direction);
+        }
+        santa.get_visited_multiple() as i32
+    }
+
+    fn second(&self, content: String) -> i32 {
+        2
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const EXAMPLE: &str = include_str!("3_test.txt");
+    const PROD: &str = include_str!("3_prod.txt");
+
+    #[test]
+    fn first_test() {
+        let first_excersise = ThirdDay {
+            exercise: Excercise {
+                content: String::from(PROD),
+                example: String::from(EXAMPLE),
+            },
+        };
+
+        let expected_example = 4;
+        let expected_prod = 2572;
+
+        let result_example = first_excersise.solve_first(false);
+        let result_prod = first_excersise.solve_first(true);
+        assert_eq!(expected_example, result_example);
+        assert_eq!(expected_prod, result_prod);
+
+        // let expected_example = 1;
+        // let expected_prod = 1783;
+        // let result_example = first_excersise.solve_second(false);
+        // let result_prod = first_excersise.solve_second(true);
+        // assert_eq!(expected_example, result_example);
+        // assert_eq!(expected_prod, result_prod);
+    }
+}
