bd067493f93c5ba3fe35e5df4c47d47502c4a750
markkovari <kovarimarkofficial@gmail.com>
2023-05-16 05:24:53 +0200

    feat: 2015 aoc fouth day second

--- 2015/src/fourth.rs
+++ 2015/src/fourth.rs
@@ -35,7 +35,16 @@ impl Solvable for FourthDay {
     }
 
     fn second(&self, content: String) -> i32 {
-        2
+        let mut counter = 0;
+        loop {
+            let data = format!("{}{}", content, counter);
+            let calculated_hash = md5::compute(data);
+            let hash_as_string = format!("{:x}", calculated_hash);
+            if hash_as_string.starts_with("000000") {
+                return counter;
+            }
+            counter += 1;
+        }
     }
 }
 
@@ -46,6 +55,7 @@ mod tests {
     const PROD: &str = include_str!("4_prod.txt");
 
     #[test]
+    #[ignore = "Takes too long"]
     fn first_test() {
         let first_excersise = FourthDay {
             exercise: Excercise {
@@ -55,15 +65,15 @@ mod tests {
         };
 
         let expected_example = 1048970;
-        let expected_prod = 2572;
+        let expected_prod = 346386;
 
         let result_example = first_excersise.solve_first(false);
         let result_prod = first_excersise.solve_first(true);
         assert_eq!(expected_example, result_example);
         assert_eq!(expected_prod, result_prod);
 
-        let expected_example = 3;
-        let expected_prod = 2631;
+        let expected_example = 5714438;
+        let expected_prod = 9958218;
         let result_example = first_excersise.solve_second(false);
         let result_prod = first_excersise.solve_second(true);
         assert_eq!(expected_example, result_example);
