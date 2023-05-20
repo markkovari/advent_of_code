b8a4f74e816a0629676eacc11c386e9dbc76f872
markkovari <kovarimarkofficial@gmail.com>
2023-05-20 21:33:26 +0200

    feat: 2015 d8 s1-2

--- 2015/src/eigth.rs
+++ 2015/src/eigth.rs
@@ -1,9 +1,45 @@
-use std::collections::HashMap;
+use onig::Regex;
 
 use crate::{Excercise, Solvable};
 
 struct EightDay {
     exercise: Excercise,
+}
+
+pub fn raw_and_unescaped_len(s: &str) -> (usize, usize) {
+    if s.chars().nth(0) != Some('"') || s.chars().last() != Some('"') {
+        panic!("invalid format (not quoted)");
+    }
+    let raw_len = s.len();
+    let re = Regex::new(r#"\\(\\|"|x[0-9a-f]{2})"#).unwrap();
+    let ss = &s[1..s.len() - 1];
+    let (esc_count, esc_size) =
+        re.find_iter(ss)
+            .fold((0, 0), |(esc_count, esc_size), (start_pos, end_pos)| {
+                (esc_count + 1, esc_size + (end_pos - start_pos))
+            });
+    (raw_len, raw_len - 2 - esc_size + esc_count)
+}
+
+pub fn extra_chars_unescaped(text: &str) -> usize {
+    text.lines().fold(0, |extra_chars, line| {
+        let (raw_len, unescaped_len) = raw_and_unescaped_len(line);
+        extra_chars + (raw_len - unescaped_len)
+    })
+}
+
+pub fn raw_and_reescaped_len(s: &str) -> (usize, usize) {
+    let raw_len = s.len();
+    let re = Regex::new(r#"[\\"]"#).unwrap();
+    let esc_count = re.find_iter(s).count();
+    (raw_len, raw_len + 2 + esc_count)
+}
+
+pub fn extra_chars_reescaped(text: &str) -> usize {
+    text.lines().fold(0, |extra_chars, line| {
+        let (raw_len, reescaped_len) = raw_and_reescaped_len(line);
+        extra_chars + (reescaped_len - raw_len)
+    })
 }
 
 impl Solvable for EightDay {
@@ -24,11 +60,11 @@ impl Solvable for EightDay {
     }
 
     fn first(&self, content: String) -> i32 {
-        2
+        extra_chars_unescaped(&content) as i32
     }
 
     fn second(&self, content: String) -> i32 {
-        2
+        extra_chars_reescaped(&content) as i32
     }
 }
 
@@ -47,16 +83,16 @@ mod tests {
             },
         };
 
-        let expected_example = 0;
-        let expected_prod = 46065;
+        let expected_example = 12;
+        let expected_prod = 1350;
 
         let result_example = first_excersise.solve_first(false);
         let result_prod = first_excersise.solve_first(true);
         assert_eq!(expected_example, result_example);
         assert_eq!(expected_prod, result_prod);
 
-        let expected_example = 0;
-        let expected_prod = 14134;
+        let expected_example = 19;
+        let expected_prod = 2085;
         let result_example = first_excersise.solve_second(false);
         let result_prod = first_excersise.solve_second(true);
         assert_eq!(expected_example, result_example);
