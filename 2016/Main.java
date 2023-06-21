a130dc98df8ab78a784d32510995591ad680f8ec
markkovari <kovarimarkofficial@gmail.com>
2023-06-21 19:32:10 +0200

    feat(aoc-2016): day 1 part 2

--- 2016/Main.java
+++ 2016/Main.java
@@ -1,5 +1,7 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
 
 enum Direction {
   NORTH,
@@ -8,36 +10,78 @@ enum Direction {
   WEST
 }
 
+record Point(int x, int y) {
+
+}
+
 class Main {
   public static void main(String... args) {
 
     var currentDirection = Direction.NORTH;
     var currentX = 0;
     var currentY = 0;
-
+    Set<Point> visited = new HashSet<Point>();
+    visited.add(new Point(currentX, currentY));
     try {
       String content = Files.readString(Paths.get("./inputs/1/prod.data"));
-      for (String line : content.split(", ")) {
-        var direction = line.charAt(0);
-        var distance = Integer.parseInt(line.substring(1));
+      String[] lines = content.split(", ");
+      main: for (int i = 0; i < lines.length; i++) {
+        var direction = lines[i].charAt(0);
+        var distance = Integer.parseInt(lines[i].substring(1));
         switch (direction) {
           case 'R':
             switch (currentDirection) {
               case NORTH:
                 currentDirection = Direction.EAST;
-                currentX += distance;
+                for (int j = 0; j < distance; j++) {
+                  currentX += 1;
+                  Point current = new Point(currentX, currentY);
+                  if (!visited.contains(current)) {
+                    visited.add(current);
+                  } else {
+                    System.out.println(i + " " + current.toString());
+                    break main;
+                  }
+                }
                 break;
               case EAST:
                 currentDirection = Direction.SOUTH;
-                currentY -= distance;
+                for (int j = 0; j < distance; j++) {
+                  currentY -= 1;
+                  Point current = new Point(currentX, currentY);
+                  if (!visited.contains(current)) {
+                    visited.add(current);
+                  } else {
+                    System.out.println(i + " " + current.toString());
+                    break main;
+                  }
+                }
                 break;
               case SOUTH:
                 currentDirection = Direction.WEST;
-                currentX -= distance;
+                for (int j = 0; j < distance; j++) {
+                  currentX -= 1;
+                  Point current = new Point(currentX, currentY);
+                  if (!visited.contains(current)) {
+                    visited.add(current);
+                  } else {
+                    System.out.println(i + " " + current.toString());
+                    break;
+                  }
+                }
                 break;
               case WEST:
                 currentDirection = Direction.NORTH;
-                currentY += distance;
+                for (int j = 0; j < distance; j++) {
+                  currentY += 1;
+                  Point current = new Point(currentX, currentY);
+                  if (!visited.contains(current)) {
+                    visited.add(current);
+                  } else {
+                    System.out.println(i + " " + current.toString());
+                    break;
+                  }
+                }
                 break;
             }
             break;
@@ -45,19 +89,55 @@ class Main {
             switch (currentDirection) {
               case NORTH:
                 currentDirection = Direction.WEST;
-                currentX -= distance;
+                for (int j = 0; j < distance; j++) {
+                  currentX -= 1;
+                  Point current = new Point(currentX, currentY);
+                  if (!visited.contains(current)) {
+                    visited.add(current);
+                  } else {
+                    System.out.println(i + " " + current.toString());
+                    break;
+                  }
+                }
                 break;
               case EAST:
                 currentDirection = Direction.NORTH;
-                currentY += distance;
+                for (int j = 0; j < distance; j++) {
+                  currentY += 1;
+                  Point current = new Point(currentX, currentY);
+                  if (!visited.contains(current)) {
+                    visited.add(current);
+                  } else {
+                    System.out.println(i + " " + current.toString());
+                    break;
+                  }
+                }
                 break;
               case SOUTH:
                 currentDirection = Direction.EAST;
-                currentX += distance;
+                for (int j = 0; j < distance; j++) {
+                  currentX += 1;
+                  Point current = new Point(currentX, currentY);
+                  if (!visited.contains(current)) {
+                    visited.add(current);
+                  } else {
+                    System.out.println(i + " " + current.toString());
+                    break;
+                  }
+                }
                 break;
               case WEST:
                 currentDirection = Direction.SOUTH;
-                currentY -= distance;
+                for (int j = 0; j < distance; j++) {
+                  currentY -= 1;
+                  Point current = new Point(currentX, currentY);
+                  if (!visited.contains(current)) {
+                    visited.add(current);
+                  } else {
+                    System.out.println(i + " " + current.toString());
+                    break;
+                  }
+                }
                 break;
             }
             break;
@@ -66,7 +146,9 @@ class Main {
     } catch (Exception e) {
       System.out.println(e);
     }
-    var diffSimple = Math.abs(currentX) + Math.abs(currentY);
-    System.out.println("X: " + currentX + " Y: " + currentY + " Simple Distance: " + diffSimple);
+    // Part 1
+    // var diffSimple = Math.abs(currentX) + Math.abs(currentY);
+    // System.out.println("X: " + currentX + " Y: " + currentY + " Simple Distance:
+    // " + diffSimple);
   }
 }
