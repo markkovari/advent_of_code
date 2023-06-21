1435050172355d5b44793b60cec77f36781bdd65
markkovari <kovarimarkofficial@gmail.com>
2023-06-21 21:17:20 +0200

    feat(aoc_2016): day 3 part 1 and 2

--- 2016/Main.java
+++ 2016/Main.java
@@ -1,5 +1,6 @@
 import src.mark.kovari.adventofcode.First;
 import src.mark.kovari.adventofcode.Second;
+import src.mark.kovari.adventofcode.Third;
 
 class Main {
   public static void main(String... args) {
@@ -7,5 +8,7 @@ class Main {
     first.solve();
     Second second = new Second();
     second.solve();
+    Third third = new Third();
+    third.solve();
   }
 }
