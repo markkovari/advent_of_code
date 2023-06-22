26e7836cf2698e00e4bd59f9d2a3582621f0f1fc
markkovari <kovarimarkofficial@gmail.com>
2023-06-22 06:13:16 +0200

    feat(aoc-2016): day 5 part 1 and 2

--- 2016/Main.java
+++ 2016/Main.java
@@ -2,15 +2,17 @@ import src.mark.kovari.adventofcode.*;
 
 class Main {
   public static void main(String... args) {
-    First first = new First();
-    first.solve();
-    Second second = new Second();
-    second.solve();
-    Third third = new Third();
-    third.solve();
-    Fourth fourth = new Fourth();
-    fourth.solve();
-    Fifth fifth = new Fifth();
-    fifth.solve();
+    // First first = new First();
+    // first.solve();
+    // Second second = new Second();
+    // second.solve();
+    // Third third = new Third();
+    // third.solve();
+    // Fourth fourth = new Fourth();
+    // fourth.solve();
+    // Fifth fifth = new Fifth();
+    // fifth.solve();
+    Sixth sixth = new Sixth();
+    sixth.solve();
   }
 }
