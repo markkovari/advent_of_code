0599ca5e52b4a05be0e5c3e2c7c58af1152a4417
markkovari <kovarimarkofficial@gmail.com>
2023-07-01 22:30:41 +0200

    rafactor(aoc2017): add acode project

---
+++ 2017/README.md
@@ -0,0 +1,3 @@
+# 2017
+
+A description of this package.
---
+++ 2017/Sources/2017/days/First.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by markkovari on 2023. 07. 01..
+//
+
+import Foundation
+
+public func double(_ that: Int) -> Int {
+    return that * 2;
+}
