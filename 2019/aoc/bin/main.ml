c5038ec127b00fa87b1662ed0e8b1daee1551aa4
markkovari <kovarimarkofficial@gmail.com>
2023-09-21 15:23:37 +0200

    chore(aoc2019): day 8 part 1 and 2

--- 2019/aoc/bin/main.ml
+++ 2019/aoc/bin/main.ml
@@ -377,6 +377,52 @@ module Day07 = struct
       0
   let solve = (CCList.(0 -- 4) |> solve,CCList.(5 -- 9) |> solve)
 end
+
+module Day08 = struct
+  let h, w = 6, 25
+
+  type digits = { zeros : int; ones : int; twos: int }
+
+  let layers =
+    CCIO.(with_in "input/8/data" read_all)
+    |> CCString.to_list
+    |> CCList.sublists_of_len (h*w)
+
+  let count_digits x =
+    let zeros = x |> CCList.count ((=) '0') in
+    let ones  = x |> CCList.count ((=) '1') in
+    let twos  = x |> CCList.count ((=) '2') in
+    { zeros; ones; twos }
+
+  let part_1 =
+    let open CCFun in
+    List.map count_digits
+    %> List.fold_left
+      (fun (zc, res) { zeros; ones; twos } ->
+        if zeros < zc then zeros, (ones * twos) else zc, res)
+      (Int.max_int, 0)
+    %> snd
+
+  let rec pixel_color layers pixel =
+    match layers with
+    | [] -> failwith "pixel is transparent"
+    | layer :: below ->
+      (match List.nth layer pixel with
+      | '0' -> ' '
+      | '1' -> '#'
+      | '2' -> pixel_color below pixel
+      | _ -> failwith "invalid input")
+
+  let part_2 layers =
+    CCList.(0 --^ (h*w))
+    |> List.map (pixel_color layers)
+    |> CCList.sublists_of_len w
+    |> List.map CCString.of_list
+    |> List.iter (Printf.printf "%s\n")
+  
+  let solve = (layers |> part_1, layers |> part_2)
+end
+
 let (s1,s2) = Day01.solve
 let () = Printf.printf "Day 1; first: %d second: %d \n" s1 s2
 
@@ -397,3 +443,8 @@ let () = Printf.printf "Day 6; first: %d second: %d \n" s1 s2
 
 let (s1,s2) = Day07.solve
 let () = Printf.printf "Day 7; first: %d second: %d \n" s1 s2
+
+let (s1,_) = Day08.solve;;
+Day08.part_2(Day08.layers);;
+let () = Printf.printf "Day 8; first: %d second:\n" s1
+
