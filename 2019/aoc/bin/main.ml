0c86f3ca8a1474cafbcfd23980ca7c81010b9f9b
markkovari <kovarimarkofficial@gmail.com>
2023-09-22 18:30:51 +0200

    feat(aoc2019): day10 part1 and part2

--- 2019/aoc/bin/main.ml
+++ 2019/aoc/bin/main.ml
@@ -437,6 +437,81 @@ module Day09 = struct
 
   let solve = (instructions |> solve ~part:1, instructions |> solve ~part:2)
 end
+
+module Day10 = struct 
+  let asteroid_positions =
+    CCList.foldi
+      (fun acc1 row ->
+         CCList.foldi
+           (fun acc2 col c -> if c = '#' then OSeq.cons (col, row) acc2 else acc2)
+           acc1)
+      OSeq.empty
+  
+  let parse filename =
+    CCIO.(with_in filename read_lines_l)
+    |> List.map CCString.to_list
+    |> asteroid_positions
+  
+  module AngleSet = Set.Make(Float)
+  
+  let get_distance (x1, y1) (x2, y2) =
+    (x2 - x1), (y2 - y1)
+  
+  let get_angle dx dy =
+    (* note the order of parameters for atan2 is switched *)
+    atan2 (float_of_int dx) (float_of_int dy)
+  
+  
+  let find_best asteroids =
+    asteroids
+    |> OSeq.fold
+      (fun (best_coord, largest_nr) a ->
+         asteroids
+         |> OSeq.fold
+           (fun angles_seen b ->
+              if a <> b then
+                let dx, dy = get_distance a b in
+                let phi = get_angle dx dy in
+                AngleSet.add phi angles_seen
+              else
+                angles_seen)
+           AngleSet.empty
+         |> AngleSet.cardinal
+         |> (fun n ->
+             if n > largest_nr then (a, n)
+             else (best_coord, largest_nr)) )
+      ((0, 0), 0)
+  
+  
+  let relative_locations station =
+    OSeq.fold
+      (fun acc other ->
+         let dx, dy = get_distance station other in
+         let phi = get_angle dx dy in
+         OSeq.cons (phi, (dx, dy)) acc)
+      OSeq.empty
+  
+  
+  let find_200th station asterioids =
+    let angle_cmp (phi1, _) (phi2, _) = - Float.compare phi1 phi2 in
+    asterioids
+    |> relative_locations station
+    |> OSeq.sort_uniq angle_cmp
+    |> OSeq.nth 199
+    |> (fun (_, (other_x, other_y)) ->
+        let (station_x, station_y) = station in
+        100 * (station_x + other_x) + (station_y + other_y))
+  
+  
+  let asteroids = parse "input/10/data"
+  
+  let monitoring_station, nr_of_visible = asteroids |> find_best
+  let pos_of_200th = asteroids |> find_200th monitoring_station
+
+  let solve = (nr_of_visible, pos_of_200th)
+end
+
+
 let (s1,s2) = Day01.solve
 let () = Printf.printf "Day 1; first: %d second: %d \n" s1 s2
 
@@ -466,3 +541,7 @@ let () = Printf.printf "Day 8; first: %d second:\n" s1
 
 let (s1,s2) = Day09.solve
 let () = Printf.printf "Day 9; first: %d second: %d \n" s1 s2
+
+let (s1,s2) = Day10.solve
+let () = Printf.printf "Day 10; first: %d second: %d \n" s1 s2
+
