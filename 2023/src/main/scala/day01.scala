99df3329496803c9a782866fff6e8d16f7739866
markkovari <kovarimarkofficial@gmail.com>
2023-12-16 23:54:15 +0100

    refactor: remove extra parenthesis

--- 2023/src/main/scala/day01.scala
+++ 2023/src/main/scala/day01.scala
@@ -6,8 +6,8 @@ import scala.io.Source
 @main def part1: Unit = println(s"The solution is ${firstResult}")
 @main def part2: Unit = println(s"The solution is ${secondResult}")
 
-val numbersAsStringsAndValues =
-  HashMap[String, Int](
+private val numbersAsStringsAndValues =
+  HashMap(
     "one" -> 1,
     "two" -> 2,
     "three" -> 3,
@@ -19,11 +19,10 @@ val numbersAsStringsAndValues =
     "nine" -> 9
   )
 
-private def stringStartsStringifiedDigit(text: String): Option[Int] = {
+private def stringStartsStringifiedDigit(text: String): Option[Int] =
   numbersAsStringsAndValues
     .find { case (key, _) => text.startsWith(key) }
     .map { case (_, value) => value }
-}
 
 private val exampleFilename = "example_1"
 private val exampleFilename2 = "example_2"
@@ -31,6 +30,7 @@ private val valuesFilename = "values"
 
 private val lines =
   Source.fromFile(s"./src/main/resources/1/${valuesFilename}").getLines
+
 private val linesForSecond =
   Source.fromFile(s"./src/main/resources/1/${valuesFilename}").getLines
 
@@ -44,39 +44,34 @@ val secondResult =
     .map(line => getFirstAndLastMultipliedTen(getMixedUpDigits(line)))
     .sum
 
-private def getFirstAndLastMultipliedTen(list: List[Int]): Int = {
+private def getFirstAndLastMultipliedTen(list: List[Int]): Int =
   list match {
     case a :: Nil       => a * 10 + a
     case a :: b :: Nil  => a * 10 + b
     case a :: b :: tail => a * 10 + tail.last
     case _              => 0
   }
-}
 
 def getMixedUpDigits(of: String): List[Int] = of match {
   case "" => List()
-  case other => {
+  case other =>
     stringStartsStringifiedDigit(other) match {
       case Some(value) => value :: getMixedUpDigits(other.splitAt(1)._2)
-      case None => {
+      case None =>
         val (head, tail) = other.splitAt(1)
         head.toIntOption match {
-          case None    => getMixedUpDigits(tail)
-          case Some(_) => head.toInt :: getMixedUpDigits(tail)
+          case None        => getMixedUpDigits(tail)
+          case Some(value) => value :: getMixedUpDigits(tail)
         }
-      }
     }
-  }
-
 }
 
 def getDigits(of: String): List[Int] = of match {
   case "" => List()
-  case a => {
+  case a =>
     val (head, tail) = a.splitAt(1)
     head.toIntOption match {
-      case None    => getDigits(tail)
-      case Some(_) => head.toInt :: getDigits(tail)
+      case None        => getDigits(tail)
+      case Some(value) => value :: getDigits(tail)
     }
-  }
 }
