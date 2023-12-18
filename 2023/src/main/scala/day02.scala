649d22540130fb481b6e763946a7ccd8d10087e0
markkovari <kovarimarkofficial@gmail.com>
2023-12-18 14:50:58 +0100

    feat(aoc-2023): day 2 part 1

--- 2023/src/main/scala/day02.scala
+++ 2023/src/main/scala/day02.scala
@@ -1,95 +1,50 @@
 package com.markkovari.adventofcode.day02
 
-import scala.collection.immutable.HashMap
-import scala.io.Source
-import scala.annotation.static
 
-@main def part1: Unit = println(s"The solution is ${firstResult}")
-@main def part2: Unit = println(s"The solution is ${secondResult}")
+case class Colors(color: String, count: Int)
+case class Game(id: Int, hands: List[List[Colors]])
+type Summary = Game => Int
 
-private val exampleFilename = "example_1"
-private val valuesFilename = "values"
+def parseColors(pair: String): Colors =
+  val Array(count0, color0) = pair.split(" ")
+  Colors(color = color0, count = count0.toInt)
 
-private val lines =
-  Source.fromFile(s"./src/main/resources/2/${exampleFilename}").getLines
+def parse(line: String): Game =
+  val Array(game0, hands) = line.split(": "): @unchecked
+  val Array(_, id) = game0.split(" "): @unchecked
+  val hands0 = hands.split("; ").toList
+  val hands1 = hands0.map(_.split(", ").map(parseColors).toList)
+  Game(id = id.toInt, hands = hands1)
 
-private val linesForSecond =
-  Source.fromFile(s"./src/main/resources/2/${valuesFilename}").getLines
+def solution(input: String, summarise: Summary): Int =
+  input.linesIterator.map(parse andThen summarise).sum
 
-val firstResult =
-  getGamesFromFile(lines)
-    .filter(game =>
-      // Add all cubes together
-      game
-        .addSetsTogether()
-        .sets
-        .head
-        .tapEach(cube => println(cube))
-        .isSubsetOf(existingCubes)
-    )
-    .length
+val possibleCubes = Map(
+  "red" -> 12,
+  "green" -> 13,
+  "blue" -> 14
+)
 
-val secondResult =
-  2
+def validGame(game: Game): Boolean =
+  game.hands.forall: hand =>
+    hand.forall:
+      case Colors(color, count) =>
+        count <= possibleCubes.getOrElse(color, 0)
 
-trait Comparable[T] {
-  def compare(that: T): Int
-}
+val possibleGame: Summary =
+  case game if validGame(game) => game.id
+  case _                       => 0
 
-class Cube(val amount: Int, val color: String) {
-  override def toString: String = s"${this.amount} ${this.color}"
-}
+def part1(input: String): Int = solution(input, possibleGame)
 
-def createCubeFromString(cubeString: String): Cube = {
-  val (amount, color) = cubeString.split(" ").toList.splitAt(1)
-  new Cube(amount.head.toInt, color.head)
-}
+val initial = Seq("red", "green", "blue").map(_ -> 0).toMap
 
-case class Game(val index: Int, val sets: List[List[Cube]]) {
-  def addSetsTogether(): Game =
-    new Game(this.index, List(this.sets.flatten))
+def minimumCubes(game: Game): Int =
+  var maximums = initial
+  for
+    hand <- game.hands
+    Colors(color, count) <- hand
+  do maximums += (color -> (maximums(color) `max` count))
+  maximums.values.product
 
-  override def toString: String =
-    s"Game ${this.index}: ${this.sets.map(set => set.mkString(", ")).mkString("; ")}"
-}
-
-extension (cubes: List[Cube]) {
-  def +(other: List[Cube]): List[Cube] =
-    cubes ++ other
-
-  def isSubsetOf(other: List[Cube]): Boolean =
-    if other == null then false
-    else if (cubes.length > other.length) false
-    else
-      cubes.forall(cube =>
-        other
-          .filter(otherCube => otherCube.color == cube.color)
-          .exists(otherCube => otherCube.amount >= cube.amount)
-      )
-}
-
-def createGameFromString(gameString: String): Game = {
-  val stripped = gameString.stripPrefix("Game ")
-  val (head, tail) = stripped.split(": ").toList.splitAt(1)
-  val index = head.head.toInt
-  val sets = tail.head
-    .split("; ")
-    .map(set =>
-      set
-        .split(", ")
-        .map(cube => createCubeFromString(cube))
-        .toList
-    )
-    .toList
-  new Game(index, sets)
-}
-
-def getGamesFromFile(lines: Iterator[String]): List[Game] =
-  lines.map(line => createGameFromString(line)).toList
-
-val existingCubes: List[Cube] =
-  List(
-    new Cube(12, "red"),
-    new Cube(13, "green"),
-    new Cube(14, "blue")
-  )
+def part2(input: String): Int = solution(input, minimumCubes)
--- 2023/src/test/scala/day02.scala
+++ 2023/src/test/scala/day02.scala
@@ -1,10 +1,24 @@
 package com.markkovari.adventofcode
 
 import org.scalatest.funsuite.AnyFunSuite
+import scala.io.Source
+import scala.collection.immutable.HashMap
+import day02._
 
 class Day02Tests extends AnyFunSuite {
 
-  test("'double' should handle 0") {
-    assert(1 == 1)
+  test("examples result is the same as in the description") {
+
+    val text = Source.fromFile("./src/main/resources/2/example_1").mkString
+    val result = part1(text)
+    assert(result == 8)
   }
+
+  test("result is the same as in the description") {
+
+    val text = Source.fromFile("./src/main/resources/2/values").mkString
+    val result = part1(text)
+    assert(result == 2617)
+  }
+
 }
