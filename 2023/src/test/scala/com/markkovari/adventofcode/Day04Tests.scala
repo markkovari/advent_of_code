7757e426ebdfd9a138d76362fb6ccc1da3da36f6
Mark Kovari <kovarimarkofficial@gmail.com>
2024-05-28 19:23:03 +0200

    chore: using using rather than dangling resources

--- 2023/src/test/scala/com/markkovari/adventofcode/Day04Tests.scala
+++ 2023/src/test/scala/com/markkovari/adventofcode/Day04Tests.scala
@@ -4,12 +4,11 @@ import com.markkovari.adventofcode.day04.*
 import org.scalatest.funsuite.AnyFunSuite
 
 import scala.io.Source
+import scala.util.Using
 
 class Day04Tests extends AnyFunSuite {
 
   test("Card is parsed correctly") {
-
-    // Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
     val input = "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
     val card = parseCard(input)
     assert(card.id == 6)
@@ -18,29 +17,37 @@ class Day04Tests extends AnyFunSuite {
   }
 
   test("Card is scored correctly") {
-    val example = Source.fromFile("./src/test/resources/4/example_1").mkString
-    val cards = parseCards(example)
-    val scores = cards.map(_.getScore())
-    assert(scores == List(8, 2, 2, 1, 0, 0))
+    Using(Source.fromFile("./src/test/resources/4/example_1")) { source =>
+      val example = source.mkString
+      val cards = parseCards(example)
+      val scores = cards.map(_.getScore())
+      assert(scores == List(8, 2, 2, 1, 0, 0))
+    }
   }
 
   test("First solution") {
-    val example = Source.fromFile("./src/test/resources/4/values").mkString
-    val cards = parseCards(example)
-    val scores = cards.map(_.getScore())
-    assert(scores.sum == 22193)
+    Using(Source.fromFile("./src/test/resources/4/values")) { source =>
+      val example = source.mkString
+      val cards = parseCards(example)
+      val scores = cards.map(_.getScore())
+      assert(scores.sum == 22193)
+    }
   }
 
   test("Example part2") {
-    val example = Source.fromFile("./src/test/resources/4/example_1").mkString
-    val ticketAmount = part2(example)
-    assert(ticketAmount - 1 == 30)
+    Using(Source.fromFile("./src/test/resources/4/example_1")) { source =>
+      val example = source.mkString
+      val ticketAmount = part2(example)
+      assert(ticketAmount == 29)
+    }
   }
 
   test("Solution part2") {
-    val example = Source.fromFile("./src/test/resources/4/values").mkString
-    val ticketAmount = part2_(example)
-    assert(ticketAmount == "5625994")
+    Using(Source.fromFile("./src/test/resources/4/values")) { source =>
+      val example = source.mkString
+      val ticketAmount = part2_(example)
+      assert(ticketAmount == "5625994")
+    }
   }
 
 }
