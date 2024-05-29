6911ace310ae5dc22300d060048061c811ffb06e
Mark Kovari <kovarimarkofficial@gmail.com>
2024-05-29 20:43:06 +0200

    feat(aoc-2023): add day 6 part 1 and part 2

--- 2023/src/test/scala/com/markkovari/adventofcode/Day06Tests.scala
+++ 2023/src/test/scala/com/markkovari/adventofcode/Day06Tests.scala
@@ -1,6 +1,6 @@
 package com.markkovari.adventofcode
 
-import com.markkovari.adventofcode.day06.part1
+import com.markkovari.adventofcode.day06.*
 import org.scalatest.funsuite.AnyFunSuite
 
 import scala.io.Source
@@ -9,6 +9,24 @@ class Day06Tests extends AnyFunSuite {
   test("Part1 example") {
     val source = io.Source.fromFile("./src/test/resources/6/example_1")
     assert(part1(source.mkString) === 288)
+    source.close()
+  }
+
+  test("Part1 solution") {
+    val source = io.Source.fromFile("./src/test/resources/6/values")
+    assert(part1(source.mkString) === 4403592)
+    source.close()
+  }
+
+  test("Part2 example") {
+    val source = io.Source.fromFile("./src/test/resources/6/example_1")
+    assert(part2(source.mkString) === 71503)
+    source.close()
+  }
+
+  test("Part2 solution") {
+    val source = io.Source.fromFile("./src/test/resources/6/values")
+    assert(part2(source.mkString) === 38017587)
     source.close()
   }
 }
