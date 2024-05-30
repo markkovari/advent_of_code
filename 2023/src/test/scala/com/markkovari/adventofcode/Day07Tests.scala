b2d17e5ba3cfc62b5a7a6ecbfe499f84ba8bf6ac
Mark Kovari <kovarimarkofficial@gmail.com>
2024-05-30 15:06:35 +0200

    feat(aoc-2023): day 07 part 1 and part 2  ... not the slightest idea what is going on tho

---
+++ 2023/src/test/scala/com/markkovari/adventofcode/Day07Tests.scala
@@ -0,0 +1,32 @@
+package com.markkovari.adventofcode
+
+import com.markkovari.adventofcode.day07.{part1, part2}
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day07Tests extends AnyFunSuite {
+
+  test("Part1 example") {
+    val source = io.Source.fromFile("./src/test/resources/7/example_1")
+    assert(part1(source.mkString) === 6440)
+    source.close()
+
+  }
+
+  test("Part1 solution") {
+    val source = io.Source.fromFile("./src/test/resources/7/values")
+    assert(part1(source.mkString) === 250602641)
+    source.close()
+  }
+
+  test("Part2 example") {
+    val source = io.Source.fromFile("./src/test/resources/7/example_1")
+    assert(part2(source.mkString) === 5905)
+    source.close()
+  }
+
+  test("Part2 solution") {
+    val source = io.Source.fromFile("./src/test/resources/7/values")
+    assert(part2(source.mkString) === 251037509)
+    source.close()
+  }
+}
