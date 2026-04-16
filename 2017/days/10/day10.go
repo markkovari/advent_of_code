3e92042af81bdb1f39bf3bbd104b4e4507a0d357
markkovari <kovarimarkofficial@gmail.com>
2026-04-16 04:33:08 +0200

    feat(2017): refactor days 10-11
    Refactored Days 10 and 11 of 2017 to use the common framework. Successfully migrated solutions and integrated them into the runner.

---
+++ 2017/days/10/day10.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"github.com/markkovari/advent_of_code/aoc-go-common"
+)
+
+type Day10 struct{}
+
+func (d Day10) Part1(input string) string {
+	list := make([]int, 256)
+	for i := range list { list[i] = i }
+	lengths := parse(input)
+	pos, skip := 0, 0
+	for _, l := range lengths {
+		reverse(&list, pos, l)
+		pos = (pos + l + skip) % 256
+		skip++
+	}
+	return fmt.Sprintf("%d", list[0]*list[1])
+}
+
+func (d Day10) Part2(input string) string {
+	list := make([]int, 256)
+	for i := range list { list[i] = i }
+	lengths := append([]int(nil), input...)
+	lengths = append(lengths, []int{17, 31, 73, 47, 23}...)
+	pos, skip := 0, 0
+	for round := 0; round < 64; round++ {
+		for _, l := range lengths {
+			reverse(&list, pos, l)
+			pos = (pos + l + skip) % 256
+			skip++
+		}
+	}
+	var hash string
+	for i := 0; i < 16; i++ {
+		block := 0
+		for j := 0; j < 16; j++ { block ^= list[i*16+j] }
+		hash += fmt.Sprintf("%02x", block)
+	}
+	return hash
+}
+
+func reverse(list *[]int, pos, l int) {
+	for i := 0; i < l/2; i++ {
+		a, b := (pos+i)%256, (pos+l-1-i)%256
+		(*list)[a], (*list)[b] = (*list)[b], (*list)[a]
+	}
+}
+
+func parse(input string) []int {
+	var l []int
+	for _, s := range strings.Split(strings.TrimSpace(input), ",") {
+		v, _ := strconv.Atoi(s)
+		l = append(l, v)
+	}
+	return l
+}
+
+func main() { common.Run(2017, 10, Day10{}) }
---
+++ 2017/days/11/day11.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+	"github.com/markkovari/advent_of_code/aoc-go-common"
+)
+
+type Day11 struct{}
+
+func (d Day11) Part1(input string) string {
+	d1, d2 := solve(input)
+	return fmt.Sprintf("%d", d1)
+}
+
+func (d Day11) Part2(input string) string {
+	_, d2 := solve(input)
+	return fmt.Sprintf("%d", d2)
+}
+
+func solve(input string) (int, int) {
+	x, y, z := 0, 0, 0
+	maxD := 0
+	for _, dir := range strings.Split(strings.TrimSpace(input), ",") {
+		switch dir {
+		case "n": y++; z--
+		case "s": y--; z++
+		case "ne": x++; z--
+		case "sw": x--; z++
+		case "nw": x--; y++
+		case "se": x++; y--
+		}
+		dist := (abs(x) + abs(y) + abs(z)) / 2
+		if dist > maxD { maxD = dist }
+	}
+	return (abs(x) + abs(y) + abs(z)) / 2, maxD
+}
+
+func abs(x int) int { if x < 0 { return -x }; return x }
+
+func main() { common.Run(2017, 11, Day11{}) }
