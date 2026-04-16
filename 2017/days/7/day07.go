d2043c74ccec3d07bfe846be51d7900510424a98
markkovari <kovarimarkofficial@gmail.com>
2026-04-16 04:30:45 +0200

    feat(2017): refactor day 7
    Refactored Day 7 of 2017 to use the common framework. Successfully migrated solution and integrated it into the runner.

---
+++ 2017/days/7/day07.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/markkovari/advent_of_code/aoc-go-common"
+)
+
+type Day07 struct{}
+
+type Node struct {
+	Name   string
+	Weight int
+	Edges  []string
+}
+
+func (d Day07) Part1(input string) string {
+	graph := parse(input)
+	allNames := make(map[string]bool)
+	for k := range graph { allNames[k] = true }
+	for _, node := range graph {
+		for _, name := range node.Edges {
+			delete(allNames, name)
+		}
+	}
+	var nameAtBottom string
+	for name := range allNames { nameAtBottom = name }
+	return nameAtBottom
+}
+
+func (d Day07) Part2(input string) string {
+	graph := parse(input)
+	currentNode := d.Part1(input)
+	weightCalculator := calcWeight(graph)
+
+	var siblings []string
+	for {
+		weightToDependents := make(map[int][]string)
+		for _, dependentName := range graph[currentNode].Edges {
+			weight := weightCalculator(dependentName)
+			weightToDependents[weight] = append(weightToDependents[weight], dependentName)
+		}
+
+		if len(weightToDependents) > 1 {
+			siblings = graph[currentNode].Edges
+			for _, names := range weightToDependents {
+				if len(names) == 1 {
+					currentNode = names[0]
+				}
+			}
+		} else if len(weightToDependents) == 1 {
+			currentWeight := weightCalculator(currentNode)
+			for _, sib := range siblings {
+				if sib != currentNode {
+					desiredWeight := weightCalculator(sib)
+					return fmt.Sprintf("%d", graph[currentNode].Weight-(currentWeight-desiredWeight))
+				}
+			}
+		} else {
+			break
+		}
+	}
+	return "0"
+}
+
+func parse(input string) map[string]Node {
+	lines := strings.Split(input, "\n")
+	graph := make(map[string]Node)
+	re := regexp.MustCompile(`([a-z]+) \((\d+)\)(?: -> (.*))?`)
+	for _, l := range lines {
+		if l == "" { continue }
+		caps := re.FindStringSubmatch(l)
+		name := caps[1]
+		weight, _ := strconv.Atoi(caps[2])
+		var edges []string
+		if caps[3] != "" {
+			edges = strings.Split(caps[3], ", ")
+		}
+		graph[name] = Node{Name: name, Weight: weight, Edges: edges}
+	}
+	return graph
+}
+
+func calcWeight(graph map[string]Node) func(string) int {
+	memo := make(map[string]int)
+	var closureFunc func(string) int
+	closureFunc = func(rootName string) int {
+		if wt, ok := memo[rootName]; ok { return wt }
+		sum := graph[rootName].Weight
+		for _, dependent := range graph[rootName].Edges {
+			sum += closureFunc(dependent)
+		}
+		memo[rootName] = sum
+		return sum
+	}
+	return closureFunc
+}
+
+func main() {
+	common.Run(2017, 7, Day07{})
+}
