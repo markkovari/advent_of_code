86db23518313a436ed68615791f2142ce5d4aa15
markkovari <kovarimarkofficial@gmail.com>
2026-04-16 03:58:52 +0200

    feat(2022): refactor day 6
    Refactored Day 6 of 2022 to use the common framework. Successfully migrated solution and integrated it into the runner.

---
+++ 2022/src/day06.rs
@@ -0,0 +1,35 @@
+use aoc_rust_common::Solution;
+use std::fmt::Display;
+use std::collections::HashSet;
+
+pub struct Day06;
+
+fn are_unique(elements: &[char]) -> bool {
+    let mut unique_elements: HashSet<char> = HashSet::new();
+    for &element in elements {
+        if !unique_elements.insert(element) {
+            return false;
+        }
+    }
+    true
+}
+
+fn solve(input: &str, window_size: usize) -> usize {
+    input.chars().collect::<Vec<char>>()
+        .windows(window_size)
+        .position(|w| are_unique(w))
+        .unwrap() + window_size
+}
+
+impl Solution for Day06 {
+    fn year(&self) -> u32 { 2022 }
+    fn day(&self) -> u32 { 6 }
+
+    fn part1(&self, input: &str) -> Box<dyn Display> {
+        Box::new(solve(input, 4))
+    }
+
+    fn part2(&self, input: &str) -> Box<dyn Display> {
+        Box::new(solve(input, 14))
+    }
+}
--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -3,12 +3,14 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 use aoc_rust_common::run_solution;
 use day01::Day01;
 use day02::Day02;
 use day03::Day03;
 use day04::Day04;
 use day05::Day05;
+use day06::Day06;
 
 fn main() {
     run_solution(Day01);
@@ -16,4 +18,5 @@ fn main() {
     run_solution(Day03);
     run_solution(Day04);
     run_solution(Day05);
+    run_solution(Day06);
 }
