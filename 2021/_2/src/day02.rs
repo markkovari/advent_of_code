3fae2a70e7fa296d80a6055e47f0c828799997dd
markkovari <kovarimarkofficial@gmail.com>
2026-04-16 03:42:32 +0200

    feat(2021): refactor day 2
    Refactored Day 2 of 2021 to use the common framework. Successfully migrated solution, updated project structure, and integrated it into the runner.

---
+++ 2021/_2/src/day02.rs
@@ -0,0 +1,39 @@
+use aoc_rust_common::Solution;
+use std::fmt::Display;
+
+pub struct Day02;
+
+impl Solution for Day02 {
+    fn year(&self) -> u32 { 2021 }
+    fn day(&self) -> u32 { 2 }
+
+    fn part1(&self, input: &str) -> Box<dyn Display> {
+        let (x, y, _) = input.lines().fold((0, 0, 0), |(px, py, aim), line| {
+            let parts: Vec<&str> = line.split(' ').collect();
+            let d = parts[0].chars().next().unwrap();
+            let w: i32 = parts[1].parse().unwrap();
+            match d {
+                'f' => (px + w, py, aim),
+                'u' => (px, py, aim - w),
+                'd' => (px, py, aim + w),
+                _ => (px, py, aim),
+            }
+        });
+        Box::new(x * y)
+    }
+
+    fn part2(&self, input: &str) -> Box<dyn Display> {
+        let (x, y, _) = input.lines().fold((0, 0, 0), |(px, py, aim), line| {
+            let parts: Vec<&str> = line.split(' ').collect();
+            let d = parts[0].chars().next().unwrap();
+            let w: i32 = parts[1].parse().unwrap();
+            match d {
+                'f' => (px + w, py + aim * w, aim),
+                'u' => (px, py, aim - w),
+                'd' => (px, py, aim + w),
+                _ => (px, py, aim),
+            }
+        });
+        Box::new(x * y)
+    }
+}
