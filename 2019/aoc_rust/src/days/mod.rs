da7f52c3311597cf9d4801e7069126867f62b370
markkovari <kovarimarkofficial@gmail.com>
2026-04-16 03:40:40 +0200

    feat(2019): refactor day 14
    Refactored Day 14 for 2019 to use the common framework. Simplified the implementation by removing unused code and adapting the core logic to the Solution trait.

--- 2019/aoc_rust/src/days/mod.rs
+++ 2019/aoc_rust/src/days/mod.rs
@@ -1,3 +1,3 @@
 pub mod day13;
-// pub mod day14;
+pub mod day14;
 // pub mod day15;
--- 2019/aoc_rust/src/main.rs
+++ 2019/aoc_rust/src/main.rs
@@ -4,6 +4,9 @@ mod utils;
 use aoc_rust_common::run_solution;
 use days::day13::Day13;
 
+use days::day14::Day14;
+
 fn main() {
     run_solution(Day13);
+    run_solution(Day14);
 }
