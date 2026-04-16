23b309910d6eee328d9baf6e7ba214ea4bf1abc9
markkovari <kovarimarkofficial@gmail.com>
2026-04-16 04:06:33 +0200

    feat(2022): refactor day 16
    Refactored Day 16 of 2022 to use the common framework. Successfully migrated solution, updated project structure, and integrated it into the runner.

---
+++ 2022/src/day16.rs
@@ -0,0 +1,110 @@
+use aoc_rust_common::Solution;
+use std::fmt::Display;
+use std::collections::HashMap;
+use sscanf::sscanf;
+
+pub struct Day16;
+
+#[derive(Debug)]
+struct Valve<'a> {
+    name: &'a str,
+    flow: u16,
+    tunnels: Vec<&'a str>,
+}
+
+impl Solution for Day16 {
+    fn year(&self) -> u32 { 2022 }
+    fn day(&self) -> u32 { 16 }
+
+    fn part1(&self, input: &str) -> Box<dyn Display> {
+        let valves = parse(input);
+        let dists = compute_dists(&valves);
+        let non_zero = valves.iter().filter(|v| v.flow > 0).map(|v| v.name).collect::<Vec<_>>();
+        let start = "AA";
+        
+        let mut memo = HashMap::new();
+        Box::new(max_pressure(start, 30, 0, &non_zero, &dists, &valves, &mut memo))
+    }
+
+    fn part2(&self, input: &str) -> Box<dyn Display> {
+        let valves = parse(input);
+        let dists = compute_dists(&valves);
+        let non_zero = valves.iter().filter(|v| v.flow > 0).map(|v| v.name).collect::<Vec<_>>();
+        let start = "AA";
+        
+        let mut memo = HashMap::new();
+        Box::new(max_pressure_two(start, start, 26, 26, 0, &non_zero, &dists, &valves, &mut memo))
+    }
+}
+
+fn parse(input: &str) -> Vec<Valve> {
+    input.lines().map(|line| {
+        let (name, flow, _, tunnels) = sscanf!(line, "Valve {str} has flow rate={u16}; {str:/tunnels? leads? to valves?/} {str}").unwrap();
+        Valve { name, flow, tunnels: tunnels.split(", ").collect() }
+    }).collect()
+}
+
+fn compute_dists(valves: &[Valve]) -> HashMap<(&str, &str), u16> {
+    let mut dists = HashMap::new();
+    for v in valves {
+        for t in &v.tunnels {
+            dists.insert((v.name, *t), 1);
+        }
+        dists.insert((v.name, v.name), 0);
+    }
+    for k in valves.iter().map(|v| v.name) {
+        for i in valves.iter().map(|v| v.name) {
+            for j in valves.iter().map(|v| v.name) {
+                let d_ij = *dists.get(&(i, j)).unwrap_or(&1000);
+                let d_ik = *dists.get(&(i, k)).unwrap_or(&1000);
+                let d_kj = *dists.get(&(k, j)).unwrap_or(&1000);
+                if d_ik + d_kj < d_ij {
+                    dists.insert((i, j), d_ik + d_kj);
+                }
+            }
+        }
+    }
+    dists
+}
+
+fn max_pressure<'a>(curr: &'a str, time: u16, opened: u32, non_zero: &[&'a str], dists: &HashMap<(&str, &str), u16>, valves: &[Valve<'a>], memo: &mut HashMap<(&'a str, u16, u32), u16>) -> u16 {
+    if time == 0 { return 0; }
+    let key = (curr, time, opened);
+    if let Some(&res) = memo.get(&key) { return res; }
+
+    let mut res = 0;
+    for (i, &next) in non_zero.iter().enumerate() {
+        if (opened & (1 << i)) == 0 {
+            let dist = *dists.get(&(curr, next)).unwrap();
+            if time > dist + 1 {
+                let flow = valves.iter().find(|v| v.name == next).unwrap().flow;
+                res = res.max(flow * (time - dist - 1) + max_pressure(next, time - dist - 1, opened | (1 << i), non_zero, dists, valves, memo));
+            }
+        }
+    }
+    memo.insert(key, res);
+    res
+}
+
+fn max_pressure_two<'a>(curr1: &'a str, curr2: &'a str, time1: u16, time2: u16, opened: u32, non_zero: &[&'a str], dists: &HashMap<(&str, &str), u16>, valves: &[Valve<'a>], memo: &mut HashMap<(&'a str, &'a str, u16, u16, u32), u16>) -> u16 {
+    let key = (curr1, curr2, time1, time2, opened);
+    if let Some(&res) = memo.get(&key) { return res; }
+
+    let mut res = 0;
+    for (i, &next) in non_zero.iter().enumerate() {
+        if (opened & (1 << i)) == 0 {
+            let dist1 = *dists.get(&(curr1, next)).unwrap();
+            if time1 > dist1 + 1 {
+                let flow = valves.iter().find(|v| v.name == next).unwrap().flow;
+                res = res.max(flow * (time1 - dist1 - 1) + max_pressure_two(next, curr2, time1 - dist1 - 1, time2, opened | (1 << i), non_zero, dists, valves, memo));
+            }
+            let dist2 = *dists.get(&(curr2, next)).unwrap();
+            if time2 > dist2 + 1 {
+                let flow = valves.iter().find(|v| v.name == next).unwrap().flow;
+                res = res.max(flow * (time2 - dist2 - 1) + max_pressure_two(curr1, next, time1, time2 - dist2 - 1, opened | (1 << i), non_zero, dists, valves, memo));
+            }
+        }
+    }
+    memo.insert(key, res);
+    res
+}
